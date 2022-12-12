a1c5716bef60f8d391ef6987c2dabcb04f32b6e1
oatmealraisin0 <120403948+oatmealraisin0@users.noreply.github.com>
2022-12-12 19:17:21 +0000

    Update main.py

--- Day2/main.py
+++ Day2/main.py
@@ -1,31 +1,130 @@
-lines = open("day2\calorieInput.txt","r").readlines()
+lines = open("day2/strategyGuide.txt","r").readlines()
 
-this_elf_total = 0 #total of calories for this elf
-total_elves = 1 #count of all elves
+#Part 1:
+my_score = 0
 
-first_elf = 0 #total of first elf
-second_elf = 0 #total of 2nd elf
-third_elf = 0 #total of 3rd elf
+words = {
+	"A":"rock",
+	"B":"paper",
+	"C":"scissors",
+
+	"X":"rock",
+	"Y":"paper",
+	"Z":"scissors"
+}
+
+#string format: elf_action-my_action
+i_win = [
+	"rock-paper",
+	"paper-scissors",
+	"scissors-rock",
+]
+
+elf_win = [
+	"rock-scissors",
+	"paper-rock",
+	"scissors-paper"
+]
+
+"""
+	X rock v rock
+	I rock v paper
+	E rock v scissors
+
+	E paper v rock
+	X paper v paper
+	I paper v scissors
+
+	I scissors v rock
+	E scissors v paper
+	X scissors v scissors
+	"""
+
+action_scores = {
+	"rock":1,
+	"paper":2,
+	"scissors":3
+}
 
 for line in lines:
-	if line == "\n":#this is a gap separating elf
-		if this_elf_total > first_elf:
-			third_elf = second_elf
-			second_elf = first_elf
-			first_elf = this_elf_total
+	args = line.replace("\n","").split(" ")
 
-		elif this_elf_total > second_elf:
-			third_elf = second_elf
-			second_elf = this_elf_total
+	elf_ltr = args[0]
+	my_ltr = args[1]
 
-		elif this_elf_total > third_elf:
-			third_elf = this_elf_total
-		
-		
-		this_elf_total = 0
-		total_elves += 1
-	else:
-		this_elf_total += int(line.replace("\n",""))
+	elf_action = words[elf_ltr]
+	my_action = words[my_ltr]
+	action_code = elf_action + "-" + my_action
+
+	my_score += action_scores[my_action]#score of selected shape
+
+	if action_code in i_win:#add 6 if win
+		my_score += 6
+
+	elif action_code in elf_win:#pass if lost (don't add more)
+		pass
+
+	else:#draw, add 3
+		my_score += 3
 
 
-print(f"There were {total_elves} elves. Calorie Leaderboards:\n🥇1st: {first_elf}\n🥈2nd: {second_elf}\n🥉3rd: {third_elf}\n\nThe total is: {first_elf+second_elf+third_elf}")
+print(f"My total score was: {my_score} (Part 1)")
+
+#Part 2:
+words = {
+	"A":"rock",
+	"B":"paper",
+	"C":"scissors",
+
+	"X":"lose",
+	"Y":"draw",
+	"Z":"win"
+}
+
+#how to end based on elf input
+how_to_end = {
+	"rock":{
+		"lose":"C", #scissors
+		"draw":"A", #rock
+		"win":"B", #paper
+	},
+
+	"paper":{
+		"lose":"A", #rock
+		"draw":"B", #paper
+		"win":"C", #scissors
+	},
+
+	"scissors":{
+		"lose":"B", #paper
+		"draw":"C", #scissors
+		"win":"A", #rock
+	}
+}
+
+my_score = 0
+
+for line in lines:
+	args = line.replace("\n","").split(" ")
+
+
+	elf_action = words[args[0]]
+	win_status = words[args[1]]
+
+	my_ltr = how_to_end[elf_action][win_status]
+	my_action = words[my_ltr]
+
+	action_code = elf_action + "-" + my_action
+
+	my_score += action_scores[my_action]#score of selected shape
+
+	if win_status == "win":#add 6 if win
+		my_score += 6
+
+	elif win_status == "lose":#pass if lost (don't add more)
+		pass
+
+	else:#draw, add 3
+		my_score += 3
+
+print(f"My total score was: {my_score} (Part 2)")
